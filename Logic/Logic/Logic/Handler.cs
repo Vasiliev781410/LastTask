1de6e489b31114bc66651fcf6c928cf5b002636b
Sergei <vasilyev781410@gmail.com>
2018-05-21 12:26:47 +0300

    without spacebar

---
+++ Logic/Logic/Logic/Handler.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.IO;
+
+namespace ScanFolder1
+{
+    public interface IFinderService
+    {
+        void FindFolder(object baseDirectory);
+    }
+
+
+    public class FinderService : IFinderService
+    {
+        List<string> Folders = new List<string>();
+        public bool Stop = false;
+        object locker = new object();
+
+        public void FindFolder(object baseDirectory)
+        {
+            //ConsoleKeyInfo cki;
+            try
+            {
+                Console.WriteLine(baseDirectory.ToString());
+
+                Folders.AddRange(Directory.GetDirectories(baseDirectory.ToString()));
+                var directories = Directory.GetDirectories(baseDirectory.ToString());
+
+                foreach (var directory in directories)
+                {                   
+                    bool err = false;
+                    try
+                    {                       
+                        var directs = Directory.GetDirectories(directory.ToString());                        
+                    }
+                    catch                                
+                    {
+                        Console.WriteLine("Error of acsess for folder - {0}", directory.ToString());
+                        err = true;
+                       
+                    }
+                    if (err == false)
+                    {
+                        Thread thread = new Thread(new ParameterizedThreadStart(FindFolder));
+                        try
+                        {
+                            thread.Name = "thr" + Folders.LongCount().ToString();
+                        }
+                        catch
+                        {
+                        }
+                        Thread.Sleep(1000);
+
+                        thread.Start(directory);
+                        Console.WriteLine(Thread.CurrentThread.Name);
+                    }
+                }               
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine(e.Message);
+            }                       
+        }
+    }
+}
---
+++ ScanFolder/ScanFolder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ScanFolder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool stop = false;
+            Console.WriteLine("Enter name of folder for scan like this D:\\\\Health (for example)");
+            do
+            {
+                ScanFolder1.FinderService invoker = new ScanFolder1.FinderService();
+                string baseDirectory = Console.ReadLine();
+                if (String.IsNullOrEmpty(baseDirectory) == false)
+                {
+                   invoker.FindFolder(baseDirectory);   
+                }
+                else
+                {
+                    Console.WriteLine("This is incorrect folder's name");
+                }
+            } while (stop == false);             
+            Console.ReadLine();
+            }
+        }
+}
